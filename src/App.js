2b7689e359797e1a5cea4d4dbfa4bfa715940857
Vikas007Vikas <vikaskamineni7@gmail.com>
2023-04-28 16:01:17 -0700

    Added all Implementations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GameBoard from './components/gameboard';
+import HomePage from './components/HomePage';
+import LeaderBoard from './components/leaderboard'
+import CustomWord from './components/CustomWord';
 
 function App() {
   window.localStorage.removeItem("hangman-word");
   return (
-    <div className="App">
-      <h1>Hangman Game</h1>
-      <GameBoard word="react" maxGuesses={6} onGameOver={(win) => console.log(win)} />
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path="/gameboard" element={<GameBoard maxGuesses={6} onGameOver={(win) => console.log(win)} />} />
+          <Route exact path="/leaderboard" element={<LeaderBoard />} />
+          <Route exact path="/challenge" element={<CustomWord />} />
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+    </Router>
   );
 }
 
